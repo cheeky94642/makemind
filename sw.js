c1aabfd676b1bbf57d80fee68f6a96839810aa1b
cheeky94642 <cheeky94642@gmail.com>
2026-03-18 14:19:07 +0530

    Implement service worker event listeners
    Added event listeners for install, activate, and fetch events.

--- sw.js
+++ sw.js
@@ -1,6 +1,11 @@
-self.options = {
-    "domain": "3nbf4.com",
-    "zoneId": 10723830
-}
-self.lary = ""
-importScripts('https://3nbf4.com/act/files/service-worker.min.js?r=sw')
+self.addEventListener('install', event => {
+  console.log('Service Worker Installed');
+});
+
+self.addEventListener('activate', event => {
+  console.log('Service Worker Activated');
+});
+
+self.addEventListener('fetch', event => {
+  console.log('Fetching:', event.request.url);
+});
